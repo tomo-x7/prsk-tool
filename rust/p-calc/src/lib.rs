a1922716bc5352b83e5ab997da8f2a5a83e73f7e
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-11-09 17:43:49 +0900

    wip

--- rust/p-calc/src/lib.rs
+++ rust/p-calc/src/lib.rs
@@ -3,6 +3,8 @@ pub extern "C" fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-unsafe extern "C" {
-	a:i32;
+#[unsafe(no_mangle)]
+pub extern "C" fn malloc(size: u32) -> u32 {
+    // let arr: &[u32] = &[0; size];
+    return 1;
 }
