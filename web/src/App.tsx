d86b271f31818e926f9dbcd9b163289be5b29ada
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-11-23 17:36:32 +0900

    meta update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,44 @@
 import { lazy, type ReactNode, Suspense } from "react";
 import { FaBluesky, FaGithub } from "react-icons/fa6";
 import { createBrowserRouter, createRoutesFromChildren, Link, Outlet, Route, RouterProvider } from "react-router-dom";
-import { TitleProvider, useSetTitle, useTitle } from "./util.tsx";
+import { MetaProvider, useSetTitle, useMeta, useSetDescription } from "./util.tsx";
+import { ScaleLoader } from "react-spinners";
 
-const to = () => new Promise((resolve) => setTimeout(resolve, 0));
-const PCalc = lazy(() => to().then(() => import("./p-calc/index.tsx")));
+const to = () => new Promise((resolve) => setTimeout(resolve, 3000));
+const PCalc = lazy(
+	// () => to().then
+	(() => import("./p-calc/index.tsx")));
 
 const router = createBrowserRouter(
 	createRoutesFromChildren(
 		<Route element={<Layout />}>
-			<Route path="p-calc" element={<S e={<PCalc />} t="ポイント調整ツールβ" />} />
-			<Route index element={<Top />} />
+			<Route
+				path="p-calc"
+				element={
+					<S
+						e={<PCalc />}
+						t="ポイント調整ツールβ"
+						d="イベント編成のままでポイント調整が簡単にできるツール。"
+					/>
+				}
+			/>
+			<Route
+				index
+				element={
+					<S
+						e={<Top />}
+						t="プロセカツール集"
+						d="プロセカ関係の便利なツールとか。今はポイント調整ツールがあるよ。"
+					/>
+				}
+			/>
 			<Route path="*" element={"Not Found"} />
 		</Route>,
 	),
 );
 
 function Layout() {
-	const title = useTitle();
+	const { title } = useMeta();
 	return (
 		<div>
 			<header className="h-10 w-full flex flex-row justify-between">
@@ -25,14 +46,7 @@ function Layout() {
 				<div>{title}</div>
 				<div className="flex flex-row">
 					<a href="https://github.com/tomo-x7/prsk-tool" target="_blank" rel="noopener noreferrer">
-						<FaGithub />
-					</a>
-					<a
-						href="https://bsky.app/profile/did:plc:qcwvyds5tixmcwkwrg3hxgxd"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<FaBluesky />
+						<FaGithub size={26} />
 					</a>
 				</div>
 			</header>
@@ -43,32 +57,33 @@ function Layout() {
 	);
 }
 
-function S({ e, t }: { e: ReactNode; t: string }) {
+function S({ e, t, d }: { e: ReactNode; t: string; d: string }) {
 	useSetTitle(t);
+	useSetDescription(d);
 	return <Suspense fallback={<Loading />}>{e}</Suspense>;
 }
 function Loading() {
-	// useSetTitle("");
-	return <div>Loading...</div>;
+	return <div><ScaleLoader /></div>;
 }
 function Top() {
-	useSetTitle("プロセカツール集");
 	return (
 		<div>
-			<div>ベータ版なう</div>
-			<Link className="text-blue-600 underline" to="/p-calc">
-				ポイント調整ツール
-			</Link>
+			<div>
+				<h2>
+					<Link className="text-blue-600 underline" to="/p-calc">
+						ポイント調整ツールβ
+					</Link>
+				</h2>
+				<p>イベント編成のままでポイント調整が簡単にできるツール。</p>
+			</div>
 		</div>
 	);
 }
 
 export function App() {
 	return (
-		<TitleProvider>
-			<Suspense fallback={<div>Loading...</div>}>
-				<RouterProvider router={router} />
-			</Suspense>
-		</TitleProvider>
+		<MetaProvider>
+			<RouterProvider router={router} />
+		</MetaProvider>
 	);
 }
--- web/src/util.tsx
+++ web/src/util.tsx
@@ -1,23 +1,37 @@
 import { createContext, type PropsWithChildren, useContext, useEffect, useState } from "react";
 
-const Context = createContext<[string, (s: string) => void]>(["", () => void 0]);
+const TitleContext = createContext<[string, (s: string) => void]>(["", () => void 0]);
+const DescContext = createContext<[string, (s: string) => void]>(["", () => void 0]);
 
-export function useTitle(): string {
-	return useContext(Context)[0];
+export function useMeta() {
+	return { title: useContext(TitleContext)[0], description: useContext(DescContext)[0] };
 }
 
 export function useSetTitle(title: string) {
-	const set = useContext(Context)[1];
+	const set = useContext(TitleContext)[1];
 	useEffect(() => {
 		set(title);
 	}, [title, set]);
 }
+export function useSetDescription(description: string) {
+	const set = useContext(DescContext)[1];
+	useEffect(() => {
+		set(description);
+	}, [description, set]);
+}
 
-export function TitleProvider({ children }: PropsWithChildren) {
+export function MetaProvider({ children }: PropsWithChildren) {
 	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
 	useEffect(() => {
 		document.title = title === "プロセカツール集" ? "プロセカツール集" : `${title} - プロセカツール集`;
-		console.log("Title set to", document.title);
 	}, [title]);
-	return <Context.Provider value={[title, setTitle]}>{children}</Context.Provider>;
+	useEffect(() => {
+		document.querySelector('meta[name="description"]')?.setAttribute("content", description);
+	}, [description]);
+	return (
+		<TitleContext value={[title, setTitle]}>
+			<DescContext value={[description, setDescription]}>{children}</DescContext>
+		</TitleContext>
+	);
 }
